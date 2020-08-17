e1a547707d9fad40f72f7e66fd4b783b03ef70ee
CrioUser <crio-user@criodo.com>
2020-08-17 14:58:34 +0530

    Remove extra files

--- Storage.java
+++
@@ -1,9 +0,0 @@
-
-import java.io.IOException;
-import java.util.Map;
-
-public interface Storage {
-    public String getPassword(String username) throws IOException;
-    public Map<String, String> getUsernameToPasswordMapping() 
-        throws IOException;
-}
